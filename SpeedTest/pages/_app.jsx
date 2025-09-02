83186aad07ff259844a88e62573e5f8cc43a5442
hori39311-spec <hori39311@gmail.com>
2025-09-02 15:27:05 +0200

    GA

--- SpeedTest/pages/_app.jsx
+++ SpeedTest/pages/_app.jsx
@@ -1,6 +1,28 @@
 // pages/_app.jsx
 import '../styles/globals.css';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-0ZHMK85444"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0ZHMK85444');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
