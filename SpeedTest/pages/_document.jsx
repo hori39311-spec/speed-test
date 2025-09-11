6e7226dbc02b2ac05f947ae2677737648c2d7edb
hori39311-spec <hori39311@gmail.com>
2025-09-11 18:24:36 +0200

    favicon

--- SpeedTest/pages/_document.jsx
+++ SpeedTest/pages/_document.jsx
@@ -7,6 +7,10 @@ export default function Document() {
       <Head>
         {/* Ide kell a Google AdSense meta tag */}
         <meta name="google-adsense-account" content="ca-pub-1778433750705975" />
+        <link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" type="image/png" sizes="32x32" href="/stopwatch-favicon-32.png">
+<link rel="apple-touch-icon" href="/stopwatch-favicon-180.png">
+
       </Head>
       <body>
         <Main /> {/* Itt jelenik meg a SpeedTestApp tartalma */}
