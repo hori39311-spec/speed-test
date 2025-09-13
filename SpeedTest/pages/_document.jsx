84539c9338b0eac0a3094a34a9fe579fac40e5be
hori39311-spec <hori39311@gmail.com>
2025-09-13 22:36:38 +0200

    favicon csere _document

--- SpeedTest/pages/_document.jsx
+++ SpeedTest/pages/_document.jsx
@@ -10,8 +10,8 @@ export default function Document() {
         
         {/* Faviconok – MIND önzáró tag */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/stopwatch-favicon-32.png" />
-        <link rel="apple-touch-icon" href="/stopwatch-favicon-180.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/networkspeed-logo-favicon-32.png" />
+        <link rel="apple-touch-icon" href="/networkspeed-logo-favicon-180.png" />
       </Head>
       <body>
         <Main /> {/* Itt jelenik meg a SpeedTestApp tartalma */}
